f4b8468879f58b43bf7eeb72f13a2c95387a2536
serr <sc7227484@gmail.com>
2025-08-11 01:07:59 +0300

    some changes

--- src/base/numbers.zig
+++ src/base/numbers.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
 pub fn main() void {
-    const a: u256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+    const a: u1337 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF * 10;
     std.debug.print("{}\n{}\n", .{ a, a - 1 });
 }
--- src/base/ptrs.zig
+++ src/base/ptrs.zig
@@ -1,10 +1,17 @@
-const std = @import("std");
+const expect = @import("std").testing.expect;
 
-pub fn main() void {
-    var x: i32 = 42;
-    const ptr: *i32 = &x; // Указатель на x
+test "pointer var" {
+    const a: i32 = 42;
+    const ptr = &a;
+    try expect(ptr.* == 42);
+}
 
-    std.debug.print("x = {}\n", .{x}); // 42
-    std.debug.print("ptr = {}\n", .{ptr}); // что то типо i32@7ffe4c4a9ce8
-    std.debug.print("*ptr = {}\n", .{ptr.*}); // 42
+test "pointer arithmetic array" {
+    const array = [_]i32{ 1, 2, 3 };
+
+    // Coerce to many-item pointer
+    var ptr: [*]const i32 = &array;
+    try expect(ptr[0] == 1);
+    ptr += 2;
+    try expect(ptr[0] == 3);
 }
