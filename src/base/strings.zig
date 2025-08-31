fbb06ffda4bdd3142c86d8c38813bf69d509bdf9
serr <sc7227484@gmail.com>
2025-08-31 18:48:49 +0300

    quick update

--- src/base/strings.zig
+++ src/base/strings.zig
@@ -1,12 +1,25 @@
 const std = @import("std");
-const expect = std.testing.expect;
 const print = std.debug.print;
-const eql = std.mem.eql;
-const allocator = std.heap.page_allocator;
 
-test "init string" {
-    // Строки в Zig - это просто последовательности (массивы или срезы) байтов (u8)
-    // строковый литералы хранятся в каком то месте в памяти
-    const name = "serr";
-    try expect(@TypeOf(name) == *const [4:0]u8);
+pub fn main() void { 
+    // Напечатает *const [16:0]u8
+    // т.е. это указатель на неизменяемый массив из 16 u8 и 17-м \0
+    print(
+        "string litereal type = {s}\n",
+        .{@typeName(@TypeOf("Now i use Zigggg"))}
+    );
+
+    const str_literal: *const [16:0]u8 = "Now i use Zigggg";
+    // str_literal = "0123456789123456"; // error: cannot assign to constant
+    print(
+        "string litereal type = {s}\n",
+        .{@typeName(@TypeOf(str_literal))}
+    );
+
+    // Небольшое отступление насчет const, var
+    {
+        var str_literal_mut: *const [16:0]u8 = "Now i use Zigggg";
+        str_literal_mut = "0123456789123456";
+        // str_literal_mut[2] = "X"; // error: cannot assign to constant
+    }
 }
